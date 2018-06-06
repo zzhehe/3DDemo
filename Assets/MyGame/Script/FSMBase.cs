8fc96753e763cac8bcd2108423bc758dd6a3fc12
zzhehe <460475523@qq.com>
2018-06-06 17:12:28 +0800

    小场景

---
+++ Assets/MyGame/Script/FSMBase.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStateBase
+{
+    //能否改变状态
+    bool IsCanChange { get; set; }
+    //枚举状态
+    StateType stateType { get; }
+    void OnEnter();
+    void OnUpdate();
+    void OnFixedUpdate();
+    void OnExit();
+    
+}
+
+
+public enum StateType
+{
+    FSM_IDLE,
+    FSM_RUN,
+    FSM_DEAD,
+    FSM_WALK,
+    FSM_ADMOVE,
+    FSM_JUMP
+}
+
+public class IdleState : IStateBase
+{
+
+
+    public bool IsCanChange { get; set; }
+
+    public StateType stateType { get { return StateType.FSM_IDLE; } }
+
+    //public onEnterEvent onEnterMethod { get; private set; }
+    public delegate void onEnterEvent();
+    public event onEnterEvent onEnterMethod;
+    public delegate void onUpdateEvent();
+    public onEnterEvent onUpdateMethod;
+    public delegate void onExitEvent();
+    public onEnterEvent onExitMethod;
+    public delegate void onFixedUpdateEvent();
+    public onEnterEvent onFixedUpdateMethod;
+
+    public void OnEnter()
+    {
+        if (onEnterMethod != null)
+        {
+            onEnterMethod();
+        }
+    }
+
+    public void OnUpdate()
+    {
+        if (onUpdateMethod != null)
+        {
+            onUpdateMethod();
+        }
+    }
+
+    public void OnExit()
+    {
+        if (onExitMethod != null)
+        {
+            onExitMethod();
+        }
+    }
+
+    public void OnFixedUpdate()
+    {
+        if (onFixedUpdateMethod != null)
+        {
+            onFixedUpdateMethod();
+        }
+    }
+}
+
+public class RunState : IStateBase
+{
+    
+    public bool IsCanChange { get; set; }
+
+    public StateType stateType { get { return StateType.FSM_RUN; } }
+
+    public delegate void onEnterEvent();
+    public onEnterEvent onEnterMethod;
+    public delegate void onUpdateEvent();
+    public onEnterEvent onUpdateMethod;
+    public delegate void onExitEvent();
+    public onEnterEvent onExitMethod;
+    public delegate void onFixedUpdateEvent();
+    public onEnterEvent onFixedUpdateMethod;
+
+    public void OnEnter()
+    {
+        if (onEnterMethod != null)
+        {
+            onEnterMethod();
+        }
+    }
+
+    public void OnUpdate()
+    {
+        if (onUpdateMethod != null)
+        {
+            onUpdateMethod();
+        }
+    }
+
+    public void OnExit()
+    {
+        if (onExitMethod != null)
+        {
+            onExitMethod();
+        }
+    }
+
+    public void OnFixedUpdate()
+    {
+        if (onFixedUpdateMethod != null)
+        {
+            onFixedUpdateMethod();
+        }
+    }
+}
+
+public class FsmSystem
+{
+    List<IStateBase> statesList = new List<IStateBase>();
+    
+    public IStateBase currentState = null;
+
+    public void AddState(IStateBase state)
+    {
+        if (!statesList.Contains(state))
+        {
+            statesList.Add(state);
+        }
+    }
+
+    public void DeleteState(StateType stateType)
+    {
+        foreach (var item in statesList)
+        {
+            if (item.stateType == stateType)
+            {
+                statesList.Remove(item);
+                return;
+            }
+        }
+    }
+
+    public void ChangeState(StateType stateType)
+    {
+        foreach (var item in statesList)
+        {
+            if (item.stateType == stateType)
+            {
+                if (currentState != null && currentState.IsCanChange)
+                {
+                    currentState.OnExit();
+                    currentState = item;
+                    currentState.OnEnter();
+                }
+                else if (currentState == null)
+                {
+                    currentState = item;
+                    currentState.OnEnter();
+                }
+            }
+        }
+    }
+
+    public void UpdateState()
+    {
+        currentState.OnUpdate();
+    }
+
+    public void FixedUpdateState()
+    {
+        currentState.OnFixedUpdate();
+    }
+
+}
+
---
+++ Assets/MyGame/Script/MicInput.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicInput : MonoBehaviour {
+
+    //声音大小
+    public float volume;
+    //存储声音片段
+    AudioClip micRecord;
+    //背景音乐
+    public AudioSource BgmMusic;
+
+    string device;
+
+    public int dataLength = 128;
+	// Use this for initialization
+	void Start () {
+        //设备选择，0是默认的
+        device = Microphone.devices[0];
+        //
+        micRecord = Microphone.Start(device, true, 10, 44100);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //volume = GetMaxVolume();
+
+        volume = GetMaxVolumeByAudiosource();
+
+    }
+
+    float GetMaxVolume()
+    {
+        float maxVolume = 0f;
+
+        float[] volumeData = new float[dataLength];
+        //从哪里开始截取数据
+        int offset = Microphone.GetPosition(device) - dataLength + 1;
+        if (offset < 0)
+        {
+            return 0;
+        }
+        micRecord.GetData(volumeData, offset);
+
+        for (int i = 0; i < dataLength; i++)
+        {
+            float tempMax = volumeData[i];
+            if (maxVolume < tempMax)
+            {
+                maxVolume = tempMax;
+            }
+        }
+
+        return maxVolume;
+    }
+
+    float GetMaxVolumeByAudiosource()
+    {
+        float maxVolume = 0f;
+
+        float[] volumeData = new float[dataLength];
+
+        BgmMusic.clip.GetData(volumeData, BgmMusic.timeSamples);
+
+        for (int i = 0; i < dataLength; i++)
+        {
+            float tempMax = volumeData[i];
+            if (maxVolume < tempMax)
+            {
+                maxVolume = tempMax;
+            }
+        }
+
+        return maxVolume;
+    }
+}
